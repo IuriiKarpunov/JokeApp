417bf908e854b80add51db50ab87a8c0c09b6005
IuriiKarpunov <karpunov.yury@yandex.ru>
2023-07-07 22:22:25 +0400

    Сверстал макет

---
+++ JokeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JokeApp
+//
+//  Created by Iurii on 11.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
